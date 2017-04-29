d459538dde681580b2b0f04915b6ae3014e1c5fb
Halil Demir <dmrrhalil@gmail.com>
2017-04-30 00:22:34 +0300

    Login ekranının tüm işlemleri tamamlandı.

---
+++ Sinema/Login.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Sinema
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.lblHata = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtParola
+            // 
+            this.txtParola.Location = new System.Drawing.Point(164, 168);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.Size = new System.Drawing.Size(241, 20);
+            this.txtParola.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(99, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 19);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Parola :";
+            // 
+            // txtKullaniciAd
+            // 
+            this.txtKullaniciAd.Location = new System.Drawing.Point(164, 121);
+            this.txtKullaniciAd.Name = "txtKullaniciAd";
+            this.txtKullaniciAd.Size = new System.Drawing.Size(241, 20);
+            this.txtKullaniciAd.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(64, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Kullanıcı Ad :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(159, 59);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.label2.Size = new System.Drawing.Size(233, 29);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Sistem Girişi";
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(291, 208);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(114, 42);
+            this.btnGiris.TabIndex = 14;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // lblHata
+            // 
+            this.lblHata.AutoSize = true;
+            this.lblHata.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHata.Location = new System.Drawing.Point(160, 99);
+            this.lblHata.MaximumSize = new System.Drawing.Size(217, 19);
+            this.lblHata.MinimumSize = new System.Drawing.Size(217, 19);
+            this.lblHata.Name = "lblHata";
+            this.lblHata.Size = new System.Drawing.Size(217, 19);
+            this.lblHata.TabIndex = 15;
+            this.lblHata.Text = "Kullanıcı ad/parola yanlış girildi.";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(544, 347);
+            this.Controls.Add(this.lblHata);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKullaniciAd);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKullaniciAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Label lblHata;
+    }
+}
---
+++ Sinema/Login.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityLayer;
+using FacadeLayer;
+using BusinessLogicLayer;
+
+namespace Sinema
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            List<ECALISAN> itemlist = BLLADMIN.Calisan_SelectList();
+
+            foreach (ECALISAN item in itemlist)
+            {
+                if (item.KullaniciAd == txtKullaniciAd.Text && item.KullaniciParola == txtParola.Text && item.AdminYetki == true)
+                {
+                    Admin frmAdmin = new Admin();
+                    frmAdmin.ShowDialog();
+                    this.Close();
+                    break;
+                }
+
+                else if (item.KullaniciAd == txtKullaniciAd.Text && item.KullaniciParola == txtParola.Text && item.AdminYetki == false)
+                {
+                    Biletci frmBiletci = new Biletci();
+                    frmBiletci.ShowDialog();
+                    this.Close();
+                    break;
+                }
+
+                else
+                {
+                    lblHata.Visible = true;
+                    lblHata.ForeColor = System.Drawing.Color.Red;
+                    continue;                 
+                }              
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            lblHata.Visible = false;
+        }
+    }
+}
